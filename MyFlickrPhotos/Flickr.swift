dbf13028242e45b6a49097f1fda3e56301cf6a20
jinuman <jin35kim@gmail.com>
2019-04-02 12:45:29 +0900

    feat: Add Flickr project models

--- MyFlickrPhotos/Flickr.swift
+++ MyFlickrPhotos/Flickr.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+struct Flickr: Codable {
+    let photos: Photos
+    let stat: String
+}
+
+struct Photos: Codable {
+    let page: Int
+    let pages: Int
+    let perpage: Int
+    let total: String
+    let photo: [Photo]
+}
+
+struct Photo: Codable {
+    let id: String
+    let owner: String
+    let secret: String
+    let server: String
+    let farm: Int
+    let title: String
+    let ispublic: Int
+    let isfriend: Int
+    let isfamily: Int
+    let url: String
+    let height: String
+    let width: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, owner, secret, server, farm, title
+        case ispublic, isfriend, isfamily
+        case url = "url_m"
+        case height = "height_m"
+        case width = "width_m"
+    }
+}
