6b578a7d8614d79f756f5184099f5734662f8a9b
jinuman <jin35kim@gmail.com>
2019-04-02 12:37:14 +0900

    feat: Design and setup views

--- MyFlickrPhotos/PhotoController.swift
+++ MyFlickrPhotos/PhotoController.swift
@@ -9,8 +9,48 @@
 import UIKit
 
 class PhotoController: UIViewController {
+    
+    // MARK:- Properties
+    let photoSearchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.barTintColor = .black
+        searchBar.placeholder = "검색어를 입력하세요.."
+        return searchBar
+    }()
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.backgroundColor = .black
+        return cv
+    }()
+    
+    // MARK:- Life Cycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        title = "Photo Search"
+        view.backgroundColor = .black
+        view.clipsToBounds = true
+        
+        [photoSearchBar, collectionView].forEach {
+            view.addSubview($0)
+        }
+        
+        setupViewAutoLayouts()
+    }
+    
+    // MARK:- Setup layout methods
+    fileprivate func setupViewAutoLayouts() {
+        let guide = view.safeAreaLayoutGuide
+        photoSearchBar.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+        photoSearchBar.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+        photoSearchBar.topAnchor.constraint(equalTo: guide.topAnchor).isActive = true
+        photoSearchBar.bottomAnchor.constraint(equalTo: collectionView.topAnchor).isActive = true
+        
+        collectionView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
     }
 }
