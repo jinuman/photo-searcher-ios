4136d1d7286365fe883074b34cacc898fbf3e70e
jinuman <jin35kim@gmail.com>
2019-04-02 12:53:53 +0900

    feat: Add view model for Photo VC

--- MyFlickrPhotos/Sources/PhotoViewModel.swift
+++ MyFlickrPhotos/Sources/PhotoViewModel.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+class PhotoViewModel {
+    // MARK:- Properties
+    private(set) var photos: [Photo]
+    
+    // MARK:- Dependency Injection (DI)
+    init(photos: [Photo] = []) {
+        self.photos = photos
+    }
+    
+    // MARK:- Handling methods
+    private func photo(for indexPath: IndexPath) -> Photo {
+        return photos[indexPath.row]
+    }
+    
+    func numberOfItems() -> Int {
+        return photos.count
+    }
+    
+    func photoCellViewModel(for indexPath: IndexPath) -> PhotoCellViewModel {
+        let photo = self.photo(for: indexPath)
+        
+        return PhotoCellViewModel(imageUrl: photo.url)
+    }
+    
+    func imageUrl(for indexPath: IndexPath) -> String {
+        let photo = self.photo(for: indexPath)
+        return photo.url
+    }
+}
