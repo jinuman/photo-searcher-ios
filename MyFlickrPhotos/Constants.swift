3f7ff1f755837da2dded2e6539d03fa37af989b9
jinuman <jin35kim@gmail.com>
2019-04-02 12:44:00 +0900

    feat: Add Constants for request url

---
+++ MyFlickrPhotos/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  MyFlickrPhotos
+//
+//  Created by Jinwoo Kim on 02/04/2019.
+//  Copyright © 2019 jinuman. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+//    static let baseURL = "https://api.flickr.com/services/rest/"
+    
+    static let APIScheme = "https"
+    static let APIHost = "api.flickr.com"
+    static let APIPath = "/services/rest"
+    
+    static let APIparameters = [
+        "method" : "flickr.photos.search",
+        "api_key" : FlickrConfig.api_key,
+        "sort" : "relevance",
+        "per_page" : "10",
+        "format" : "json",
+        "nojsoncallback" : "1",
+        "extras" : "url_m"
+    ]
+}
---
+++ MyFlickrPhotos/Service.swift
@@ -0,0 +1,9 @@
+//
+//  Service.swift
+//  MyFlickrPhotos
+//
+//  Created by Jinwoo Kim on 02/04/2019.
+//  Copyright © 2019 jinuman. All rights reserved.
+//
+
+import Foundation
