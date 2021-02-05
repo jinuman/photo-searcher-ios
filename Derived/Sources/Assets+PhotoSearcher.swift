8196a8f8244d12c6a31fb4882fe83028d2328c93
Jinwoo Kim <jinumandev@gmail.com>
2021-02-05 20:56:43 +0900

    tuist edit and tuist generate

---
+++ Derived/Sources/Assets+PhotoSearcher.swift
@@ -0,0 +1,64 @@
+// swiftlint:disable all
+// Generated using tuist — https://github.com/tuist/tuist
+
+#if os(macOS)
+  import AppKit
+#elseif os(iOS)
+  import UIKit
+#elseif os(tvOS) || os(watchOS)
+  import UIKit
+#endif
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Asset Catalogs
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+public enum PhotoSearcherAsset {
+  public static let baselineSaveWhite24pt = PhotoSearcherImages(name: "baseline_save_white_24pt")
+  public static let imagePlaceholder = PhotoSearcherImages(name: "image_placeholder")
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
+// MARK: - Implementation Details
+
+public struct PhotoSearcherImages {
+  public fileprivate(set) var name: String
+
+  #if os(macOS)
+  public typealias Image = NSImage
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  public typealias Image = UIImage
+  #endif
+
+  public var image: Image {
+    let bundle = PhotoSearcherResources.bundle
+    #if os(iOS) || os(tvOS)
+    let image = Image(named: name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    let image = bundle.image(forResource: NSImage.Name(name))
+    #elseif os(watchOS)
+    let image = Image(named: name)
+    #endif
+    guard let result = image else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+}
+
+public extension PhotoSearcherImages.Image {
+  @available(macOS, deprecated,
+    message: "This initializer is unsafe on macOS, please use the PhotoSearcherImages.image property")
+  convenience init?(asset: PhotoSearcherImages) {
+    #if os(iOS) || os(tvOS)
+    let bundle = PhotoSearcherResources.bundle
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSImage.Name(asset.name))
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
---
+++ Project.swift
@@ -0,0 +1,50 @@
+import ProjectDescription
+
+let targetActions = [
+    TargetAction.pre(
+        path: "Scripts/SwiftLintRunScript.sh",
+        arguments: [],
+        name: "SwiftLint"
+    ),
+    TargetAction.pre(
+        path: "Scripts/RSwiftRunScript.sh",
+        arguments: [],
+        name: "R.Swift",
+        inputPaths: [Path.init("$TEMP_DIR/rswift-lastrun")],
+        inputFileListPaths: [],
+        outputPaths: [Path.init("$SRCROOT/TuistSample/Sources/R.generated.swift")],
+        outputFileListPaths: []
+    )
+]
+
+let project = Project(
+    name: "PhotoSearcher",
+    targets: [
+        Target(
+            name: "PhotoSearcher",
+            platform: .iOS,
+            product: .app,
+            bundleId: "com.jinuman.PhotoSearcher",
+            infoPlist: "PhotoSearcher/Supporting Files/PhotoSearcher-Info.plist",
+            sources: ["PhotoSearcher/Sources/**"],
+            resources: ["PhotoSearcher/Resources/**"],
+            dependencies: [
+                // .cocoapods(path: ".")
+            ]
+        ),
+        Target(
+            name: "PhotoSearcherTests",
+            platform: .iOS,
+            product: .unitTests,
+            bundleId: "com.jinuman.PhotoSearcherTests",
+            infoPlist: "PhotoSearcherTests/Info.plist",
+            sources: ["PhotoSearcherTests/**"],
+            dependencies: [
+                .target(name: "PhotoSearcher")
+            ]
+        )
+    ],
+    additionalFiles: [
+        "PhotoSearcher/Resources/R.generated.swift"
+    ]
+)
